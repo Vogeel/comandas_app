8883db199d76a33fbec803eab452fdfcb158b808
vitor vogel <vitor-vogel@hotmail.com>
2023-11-05 20:24:29 -0300

    Feat: adição do editar no funcionario (produto Bug

--- scr/mod_produto/produto.py
+++ scr/mod_produto/produto.py
@@ -32,10 +32,7 @@ def insert():
         nome = request.form['nome']
         descricao = request.form['descricao']
         valor_unitario = request.form['valor_unitario']
-
         foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
-
-
 
         # monta o JSON para envio a API
         payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario }
@@ -49,3 +46,46 @@ def insert():
 
     except Exception as e:
         return redirect(url_for('formListaProduto.html', msgErro=e.args[0]))
+    
+    
+@bp_produto.route("/form-edit-produto", methods=['POST'])
+def formEditProduto():
+    try:
+        id_produto = request.form['id']
+        response = requests.get(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return render_template('formProduto.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
+
+@bp_produto.route('/edit', methods=['POST'])
+def edit():
+    try:
+            id_produto = request.form['id']
+            nome = request.form['nome']
+            descricao = request.form['descricao']
+            valor_unitario = request.form['valor_unitario']
+            foto = "data:" + request.files['foto'].content_type + ";base64," + str(
+                base64.b64encode(request.files['foto'].read()), "utf-8")
+
+            payload = {
+                'id_produto': id_produto,
+                'nome': nome,
+                'descricao': descricao,
+                'foto': foto,
+                'valor_unitario': valor_unitario
+            }
+
+            response = requests.put(
+                ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API, json=payload)
+            result = response.json()
+
+            if (response.status_code != 200 or result[1] != 201):
+                raise Exception(result[0])
+
+            return redirect(url_for('produto.formListaProduto', msg=result[0]))
+
+    except Exception as e:
+            return render_template('formListaProduto.html', msgErro=e.args[0])
